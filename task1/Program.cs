c714f9c6d723adcb839ad93c0ee21e144910c7b5
darina1009 <darina_1009@mail.ru>
2023-06-20 09:09:43 +0300

    task1 and task2

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿int GenLenght(string input){
+    Console.WriteLine("Введите длину");
+     int lenght = Convert.ToInt32(Console.ReadLine());
+    return lenght;
+}
+
+int[] GetArray(int lenght){
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = GenLenght($"Введите {i + 1}-й элемент");
+        
+    }
+    return array;
+}
+
+int Count(int[] array){
+    int count = 0;
+    foreach (int num in array)
+    {
+        if(num > 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+int lenght = GenLenght("Введите количество элементов");
+int [] array = GetArray (lenght);
+string arr = String.Join(", ", array);
+Console.WriteLine(arr);
+int count = Count(array);
+Console.WriteLine(count);
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿     Console.WriteLine("Введите координату b1");
+     double  b1 = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Введите координату b2");
+     double  b2 = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Введите координату k1");
+     double  k1 = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Введите координату k2");
+   double  k2 = Convert.ToInt32(Console.ReadLine());
+
+     double x = (b2-b1)/(k1-k2);
+     double y = k1*x+b1;
+
+     Console.WriteLine($"Координата {x} и координата {y}");
+
